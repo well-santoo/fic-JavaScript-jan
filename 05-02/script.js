90a4123d92afc4536d811ef30c9b83a3affcf22b
well-santoo <lctodoroki@gmail.com>
2025-02-05 14:58:07 -0300

    Dia 05-02

---
+++ 05-02/script.js
@@ -0,0 +1,7 @@
+let nome =prompt("qual seu nome?")
+let idade =prompt("qual sua idade?")
+let time =prompt("Qual seu time?")
+
+console.log("seja bem-vindo  "  +  nome + "sua idade é:  "  + idade)
+document.write("<br> <br>");
+document.write(`Seja bem vindo ${nome}, sua idade é: ${idade} anos`)
