4330dc7184817a40ec072eca43523b75b289354b
well-santoo <lctodoroki@gmail.com>
2025-02-06 15:29:37 -0300

    Atividade

---
+++ 06-02/exer2.js
@@ -0,0 +1,12 @@
+let nome ="Welington"
+
+let livroFavorito = "Algum livro"
+
+let AutordoLivro = "autor"
+
+let Valordolivro = 50
+
+console.log ("Meu nome",  nome)
+console.log("livro",  livroFavorito)
+console.log("quem é o autor?", AutordoLivro)
+console.log(" valor dele  R$", Valordolivro)
---
+++ 06-02/exer3.js
@@ -0,0 +1,3 @@
+let linguagem = "JavaScript"
+const js = linguagem[0] + linguagem[4]
+console.log(js)
