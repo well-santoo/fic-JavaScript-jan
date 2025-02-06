48eb7826be0debd0b96cfe424d5606f7ce8b80c3
well-santoo <lctodoroki@gmail.com>
2025-02-06 14:41:49 -0300

    exercicio

---
+++ 06-02/tipos-dados.js
@@ -0,0 +1,12 @@
+let nome ="Welington"
+
+let.idade = 17
+
+let casado = false
+
+let ValorProduto = 5.35
+
+console.log=("Meu nome",  nome)
+console.log("idade",  idade)
+console.log("casado?", casado)
+console.log("R$", ValorProduto)
---
+++ 06-02/variavel.js
@@ -0,0 +1,7 @@
+let mensagem = "abobora"
+const nomecompleto = "welington santos"
+let idade = 17
+idade = 18
+
+console.log(nomecompleto)
+console.log(mensagem)
