679a6608c0088a07cbd4d651bdd1aa2828758ef5
well-santoo <lctodoroki@gmail.com>
2025-02-11 14:30:06 -0300

    Adicionando

---
+++ 11-02/programa-numero.js
@@ -0,0 +1,12 @@
+let numero = -4;
+
+if(numero > 0){
+    console.log("Positivo")
+}else if(numero < 0){
+    console.log ("Negativo")
+}else if(numero == 0){
+    console.log("Zero")
+}else{
+    console.log("Numero invalido")
+}
+
