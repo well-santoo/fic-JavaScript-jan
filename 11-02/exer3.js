53d82ec7311f963acb48c9409d95444e11b79f01
well-santoo <lctodoroki@gmail.com>
2025-02-11 16:17:32 -0300

    exercicos sobre if

---
+++ 11-02/exer3.js
@@ -0,0 +1,10 @@
+const idade= 16;
+
+let criancas = 7;
+let idosos = 65;
+
+if(idade <= criancas || idade >= idosos){
+    console.log("Para você, a entrada é gratis!")
+}else{
+    console.log("A entrada é R$30,00")
+}
---
+++ 11-02/valor2.js
@@ -0,0 +1,7 @@
+let valor = 25;
+
+if(valor<10||valor > 20){
+    console.log("Este valor é valido")
+}else{
+    console.log("Não é valido")
+}
