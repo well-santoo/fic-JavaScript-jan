69f1cd345bd01f1fb6688f5072f018addf02a1d3
well-santoo <lctodoroki@gmail.com>
2025-02-12 14:58:09 -0300

    exercicios

---
+++ 12-02/Exer2.js
@@ -0,0 +1,8 @@
+const cities = ["São paulo","Rio de janeiro", "minas gerais"]
+let upperCaseCities =[]
+console.log(cities.length)
+
+for(let i = 0; i < cities.length; i ++){
+    upperCaseCities.push(cities[i]. toUpperCase())
+}
+console.log(upperCaseCities)
---
+++ 12-02/for.js
@@ -0,0 +1,3 @@
+for (let i = 0; i <= 5; i++){
+    console.log("O valor de i, vale:" + i)
+}
