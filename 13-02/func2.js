e2829168694282a5078f4f4bd6b4123a8dfae864
well-santoo <lctodoroki@gmail.com>
2025-02-13 16:20:48 -0300

    funções

---
+++ 13-02/func2.js
@@ -0,0 +1,26 @@
+function mostrarNome(nome){
+    document.write(nome)  
+}
+
+function mostrarFrase(){
+    document.write("Seja bem-vindo")
+}
+
+
+
+function pularLinha(){
+    document.write("<br><br>")
+}
+
+mostrarNome("Fabio");
+pularLinha()
+mostrarNome("caio");
+pularLinha()
+mostrarNome("carla");
+
+
+pularLinha()
+mostrarFrase()
+
+
+
---
+++ 13-02/funções.js
@@ -0,0 +1,5 @@
+function nomeDaFuncao() {
+       alert("MInha função *******")
+}
+
+//console.log(nomeDaFuncao());
