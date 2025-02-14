d87029138bb07a4b4115ff52d0d536f3ca50581e
well-santoo <lctodoroki@gmail.com>
2025-02-14 15:48:06 -0300

    Notas

---
+++ 14-02/js/nota.js
@@ -0,0 +1,7 @@
+const inpuNota= document.getElementById("Nota")
+const resultado = document.getElementById("Resultado")
+
+function resultadoNotaClick() {
+    console.log("Ola Welington")
+    alert("Ola Welington")
+}
