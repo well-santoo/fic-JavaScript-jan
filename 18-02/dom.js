89a25cc86a6effe2a1bfe876524cc2a6bab42874
well-santoo <lctodoroki@gmail.com>
2025-02-19 14:44:44 -0300

    finalizar

--- 18-02/dom.js
+++ 18-02/dom.js
@@ -1,20 +1,40 @@
-const nome = document.getElementById("Nome")
+const nome = document.getElementById("nome")
 const cidade = document.getElementById("cidade")
-const email = document.getElementById("Email")
+const email = document.getElementById("email")
 
-const titulo =document.getElementById("titulo")
-const subtitulo =document.getElementById("subtitulos")
+const titulo = document.getElementById("titulo")
+const subtitulo = document.getElementById("subtitulos")
 const txtemail = document.getElementById("email")
 
 
 
 
-function mudar (event) {
-    event.preventDefault();
-    console.log(nome.value);
+function mudar(event) {
+    event.preventDefault()
 
-    titulo.innerHTML =nome.value;
-    subtitulo.innerHTML=subtitulo.value
+    if (nome.value === "" || cidade.value === "" || email.value === "") {
+        alert("Preencha o campo")
+        return false
+    }
+
+    titulo.innerHTML = nome.value;
+    subtitulo.innerHTML = subtitulo.value
     txtemail.innerHTML = txtemail.value
 
+    titulo.value = ""
+    subtitulo.value = ""
+    txtemail.value = ""
+
+}
+
+function mudarCor() {
+
+if(document.body.style.backgroundColor === "red";){
+    document.body.style.backgroundColor ="";
+
+}else {
+    document.style.backgroundColor = "red"
+}
+    document.body.style.backgroundColor = "red";
+   //document.body.style.backgroundColor = "red"
 }
