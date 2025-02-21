4e5c6351e0ac2b470d84abd263e44d655046c774
well-santoo <lctodoroki@gmail.com>
2025-02-21 14:43:58 -0300

    .

---
+++ 20-02/js/carros.js
@@ -0,0 +1,14 @@
+const cars =  [
+    {
+    brand: "Toyota",
+    model: "Corola",
+    year: 2022
+    },
+    {
+        brand: "Honda",
+        model: "Civic",
+        year: 2008
+        },
+]
+
+console.log(cars)
