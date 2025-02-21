800338a9ac163ce01d8342435159847e115c99a3
well-santoo <lctodoroki@gmail.com>
2025-02-21 16:14:08 -0300

    .

--- 20-02/js/carros.js
+++ 20-02/js/carros.js
@@ -8,7 +8,53 @@ const cars =  [
         brand: "Honda",
         model: "Civic",
         year: 2008
-        },
-]
+    },
+    {
+        brand: "Fit",
+        model: "uno",
+        year: 2013
+    },
+    {
+        brand: "Hyundai",
+        model: "HB20",
+        year: 2023
+    },
+    {
+        brand: "Nissan",
+        model: "march",
+        year: 2011
+    },
+    {
+        brand: "Fiat",
+        model: "Palio",
+        year: 1995
+    },
+];
 
-console.log(cars)
+function displayCard(){
+    const cardlist = document.querySelector("#card-list")
+
+    cars.forEach((car) => {
+        
+
+        const carDiv = document.createElement("div")
+        carDiv.classList.add('car')
+
+        const carModel = document.createElement("h2")
+        carModel.textContent =`${car.brand} ${car.model}`
+
+        const year = document.createElement("p")
+        year.textContent = `${car.year} `
+
+
+        
+        carDiv.appendChild(carModel)
+        carDiv.appendChild(year)
+        cardlist.appendChild(carDiv)
+
+
+    })
+
+}
+
+displayCard()
